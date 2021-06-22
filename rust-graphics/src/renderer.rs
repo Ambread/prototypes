99c99fed26629a8f71880bdc7982e3bdf152852d
jamesBeeProg <jamesbeeprog@gmail.com>
2021-06-21 20:37:42 -0400

    Add comments to renderer

--- rust-graphics/src/renderer.rs
+++ rust-graphics/src/renderer.rs
@@ -1,5 +1,4 @@
-use std::u8;
-
+use crate::{assets::Assets, chunk::Chunk};
 use anyhow::Result;
 use glfw::Context as _;
 use luminance::{
@@ -16,8 +15,7 @@ use luminance_derive::{UniformInterface, Vertex};
 use luminance_gl::gl33::GL33;
 use luminance_glfw::GlfwSurface;
 
-use crate::{assets::Assets, chunk::Chunk};
-
+// Just include the shaders as static string
 const VERTEX_SHADER: &str = include_str!("vertex.glsl");
 const FRAGMENT_SHADER: &str = include_str!("fragment.glsl");
 
@@ -31,14 +29,17 @@ pub struct Renderer {
 
 impl Renderer {
     pub fn new(surface: &mut GlfwSurface, assets: &Assets) -> Result<Self> {
+        // Use the back buffer for rendering
         let back_buffer = surface.context.back_buffer()?;
 
+        // Shader program
         let program = surface
             .context
             .new_shader_program::<VertexSemantics, (), ShaderInterface>()
             .from_strings(VERTEX_SHADER, None, None, FRAGMENT_SHADER)?
             .ignore_warnings();
 
+        // Quad that covers the entire viewport
         let quad = surface
             .context
             .new_tess()
@@ -46,6 +47,7 @@ impl Renderer {
             .set_mode(Mode::Triangle)
             .build()?;
 
+        // Texture array for all the tile sprites
         let mut tile_texture = surface.context.new_texture::<Dim2Array, NormRGB8UI>(
             (
                 assets.tile_data.texture_size.cast().unwrap().into(),
@@ -58,12 +60,15 @@ impl Renderer {
             },
         )?;
 
+        // Upload the texture array
         tile_texture.upload_raw(GenMipmaps::Yes, &assets.tile_sprites)?;
 
+        // Texture to hold the tiles of the world
         let world_texture = surface.context.new_texture::<Dim2, R8UI>(
             [Chunk::SIZE as u32, Chunk::SIZE as u32],
             0,
             Sampler {
+                // Integer textures must use `Nearest` otherwise it'll read all zeros
                 mag_filter: MagFilter::Nearest,
                 min_filter: MinFilter::Nearest,
                 ..Default::default()
@@ -109,14 +114,17 @@ impl Renderer {
                 &self.back_buffer,
                 &PipelineState::default(),
                 |pipeline, mut shade_gate| {
+                    // Any textures you have to bind here
                     let bound_texture = pipeline.bind_texture(tile_texture)?;
                     let bound_world = pipeline.bind_texture(world_texture)?;
 
                     shade_gate.shade(program, |mut interface, uniforms, mut render_gate| {
+                        // Assign uniforms
                         interface.set(&uniforms.tile_texture, bound_texture.binding());
                         interface.set(&uniforms.world_texture, bound_world.binding());
                         interface.set(&uniforms.world_size, Chunk::SIZE as u32);
 
+                        // Render some stuff
                         render_gate.render(&RenderState::default(), |mut tess_gate| {
                             tess_gate.render(&*quad)
                         })
@@ -126,6 +134,7 @@ impl Renderer {
             .assume()
             .into_result()?;
 
+        // Put the new stuff onto the screen
         surface.context.window.swap_buffers();
 
         Ok(())
@@ -161,6 +170,7 @@ struct Vertex {
     position: VertexPosition,
 }
 
+// Vertices for a quad that covers the entire viewport
 const VERTICES: [Vertex; 6] = [
     Vertex::new(VertexPosition::new([1.0, 1.0])),
     Vertex::new(VertexPosition::new([1.0, -1.0])),
