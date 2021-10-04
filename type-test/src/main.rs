5be7179607fc504f1c4197990f6f29ca7fb66875
jamesBeeProg <jamesBeeProg@gmail.com>
2021-10-04 10:53:15 -0400

    Add more expers to main

--- type-test/src/main.rs
+++ type-test/src/main.rs
@@ -1,27 +1,29 @@
 use type_test::{builder::*, data::Context};
 
 fn main() {
-    let mut context = Context::new(
-        IntoIterator::into_iter([
-            (
-                "even?".to_owned(),
-                ty_func(ty_name("Number"), ty_name("Bool")),
-            ),
-            (
-                "inc".to_owned(),
-                ty_func(ty_name("Number"), ty_name("Number")),
-            ),
-        ])
-        .collect(),
-    );
+    let env = [
+        (
+            "even?".to_owned(),
+            ty_func(ty_name("Number"), ty_name("Bool")),
+        ),
+        (
+            "inc".to_owned(),
+            ty_func(ty_name("Number"), ty_name("Number")),
+        ),
+    ];
 
-    let expr = if_else(
-        call(var("even?"), call(var("inc"), number(123))),
-        number(456),
-        number(789),
-    );
+    let exprs = [
+        if_else(
+            call(var("even?"), call(var("inc"), number(123))),
+            number(456),
+            number(789),
+        ),
+        if_else(number(123), number(123), number(123)),
+        call(var("inc"), call(var("even?"), number(123))),
+    ];
 
-    let result = expr.infer(&mut context);
-
-    dbg!(result);
+    let mut context = Context::new(IntoIterator::into_iter(env).collect());
+    for expr in exprs {
+        let _ = dbg!(expr.infer(&mut context));
+    }
 }
