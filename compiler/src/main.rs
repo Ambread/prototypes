6ae52abb4ed9680e01b84d83e0400689d3693937
jamesBeeProg <jamesBeeProg@gmail.com>
2022-01-30 10:07:07 -0500

    Take input file name from command arguments

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,18 +1,32 @@
 use anyhow::Result;
-use std::{fs, process::Command};
+use clap::Parser;
+use std::{fs, path::PathBuf, process::Command};
 
 use compiler::compile;
 
+#[derive(Debug, Parser)]
+struct Args {
+    input: PathBuf,
+}
+
 fn main() -> Result<()> {
-    let input = fs::read_to_string("wew.son")?;
+    let args = Args::parse();
+
+    let input = fs::read_to_string(&args.input)?;
     let output = compile(&input);
-    fs::write("wew.ssa", output)?;
+    fs::write(args.input.with_extension(".ssa"), output)?;
 
     Command::new("qbe")
-        .args(["wew.ssa", "-o", "wew.s"])
+        .arg(args.input.with_extension(".ssa"))
+        .arg("-o")
+        .arg(args.input.with_extension(".s"))
         .output()?;
 
-    Command::new("cc").args(["wew.s", "-o", "wew"]).output()?;
+    Command::new("cc")
+        .arg(args.input.with_extension(".s"))
+        .arg("-o")
+        .arg(args.input.file_stem().unwrap())
+        .output()?;
 
     Ok(())
 }
