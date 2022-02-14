583d98bdac0d68349741361b3ce5688244e27745
JamesBeeProg <JamesBeeProg@gmail.com>
2022-02-14 14:15:29 -0500

    Add repl commands

---
+++ interpreter/foo.es
@@ -0,0 +1,3 @@
+fn double x = x * 2;
+let y = double(2) + double(4);
+y + 2
--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,39 +1,106 @@
+use std::path::PathBuf;
+
 use anyhow::Result;
 use chumsky::Parser;
+use clap::{AppSettings, ArgEnum, FromArgMatches, IntoApp, Parser as Clap};
 use interpreter::{eval, parser};
 use rustyline::{error::ReadlineError, Editor};
 
 fn main() -> Result<()> {
     let mut rl = Editor::<()>::new();
+    let mut current_mode = Mode::Eval;
 
     loop {
-        let line = rl.readline(">> ");
+        let input = rl.readline(">> ");
 
-        if let Err(ReadlineError::Interrupted | ReadlineError::Eof) = line {
+        if let Err(ReadlineError::Interrupted | ReadlineError::Eof) = input {
             break;
         }
 
-        let line = line?;
-        rl.add_history_entry(line.as_str());
+        let input = input?;
+        rl.add_history_entry(input.as_str());
 
-        let ast = match parser().parse(line) {
-            Ok(ast) => ast,
-            Err(parse_errs) => {
-                parse_errs
-                    .into_iter()
-                    .for_each(|e| println!("Parse error: {}", e));
-                continue;
+        if input.starts_with('/') {
+            let command = match parse_command(&input) {
+                Ok(command) => command,
+                Err(err) => {
+                    eprintln!("{}", err);
+                    continue;
+                }
+            };
+
+            match command {
+                Command::Exit => break,
+                Command::Mode { mode } => current_mode = mode,
+                Command::Load { file } => {
+                    let input = std::fs::read_to_string(file)?;
+                    interpret(&input, current_mode)
+                }
             }
-        };
-
-        let vars = &mut Vec::new();
-        let funcs = &mut Vec::new();
-
-        match eval(&ast, vars, funcs) {
-            Ok(output) => println!("{}", output),
-            Err(eval_err) => println!("Evaluation error: {}", eval_err),
+            continue;
         }
+
+        interpret(&input, current_mode)
     }
 
     Ok(())
+}
+
+fn interpret(input: &str, current_mode: Mode) {
+    let ast = match parser().parse(input) {
+        Ok(ast) => ast,
+        Err(parse_errs) => {
+            parse_errs
+                .into_iter()
+                .for_each(|e| println!("Parse error: {}", e));
+            return;
+        }
+    };
+
+    if current_mode == Mode::Parse {
+        println!("{ast:#?}");
+        return;
+    }
+
+    let vars = &mut Vec::new();
+    let funcs = &mut Vec::new();
+
+    match eval(&ast, vars, funcs) {
+        Ok(output) => println!("{}", output),
+        Err(eval_err) => println!("Evaluation error: {}", eval_err),
+    }
+}
+
+#[derive(Debug, Clone, Clap)]
+enum Command {
+    Exit,
+    Mode {
+        #[clap(arg_enum)]
+        mode: Mode,
+    },
+    Load {
+        file: PathBuf,
+    },
+}
+
+#[derive(Debug, Clone, Copy, ArgEnum, PartialEq, Eq)]
+enum Mode {
+    Eval,
+    Parse,
+}
+
+fn parse_command(input: &str) -> Result<Command> {
+    let input = input.strip_prefix('/').unwrap().split_whitespace();
+
+    let mut command = Command::into_app()
+        .setting(AppSettings::DisableHelpFlag)
+        .setting(AppSettings::DisableVersionFlag)
+        .setting(AppSettings::NoAutoHelp)
+        .setting(AppSettings::NoBinaryName)
+        .setting(AppSettings::DisableHelpSubcommand);
+
+    command.set_bin_name("/");
+    let input = command.try_get_matches_from(input)?;
+
+    Ok(Command::from_arg_matches(&input)?)
 }
