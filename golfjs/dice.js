5a3e8a56d9b1c1e8c491e7627822faf19ab7b5a4
JamesBeeProg <JamesBeeProg@gmail.com>
2022-02-15 14:05:04 -0500

    dice golf

---
+++ golfjs/dice.js
@@ -0,0 +1,7 @@
+l=console.log,s=0;for(i=+prompt();i-->0;)t=~~(Math.random()*6+1),s+=t,l(t);l(s)
+
+l=console.log,s=0;for(i=prompt();i--;)s+=t=~~(6*Math.random()+1),l(t);l(s)
+
+for(i=prompt(l=alert,s=0);i--;)s+=t=~~(6*Math.random()+1),l(t);l(s)
+
+l=prompt;for(i=l(s=0);i--;)s+=t=~~(6*Math.random()+1),l(t);l(s)
