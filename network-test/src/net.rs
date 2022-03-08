063931a8e6579861e455ed42c64e1073b7013350
JamesBeeProg <JamesBeeProg@gmail.com>
2022-03-08 13:23:42 -0500

    Get net working

--- network-test/src/net.rs
+++ network-test/src/net.rs
@@ -1,61 +1,68 @@
 use anyhow::Result;
-use std::{mem::size_of, time::Duration};
+use std::mem::size_of;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
     sync::broadcast,
 };
 
-use crate::{Message, NetChannels};
+use crate::NetChannels;
 
 pub async fn server(mut channels: NetChannels) -> Result<()> {
     let (tx, mut rx) = broadcast::channel(16);
 
-    tokio::spawn(async move {
-        loop {
-            dbg!("test1");
-            let data = rx.recv().await.unwrap();
-            channels.to_game.send(data).unwrap();
-        }
-    });
+    {
+        let tx = tx.clone();
 
-    tokio::spawn(async move {
-        loop {
-            dbg!("test2");
-            let data = channels.from_game.recv().await.unwrap();
-            tx.send(data).unwrap();
-        }
-    });
+        tokio::spawn(async move {
+            loop {
+                let data = rx.recv().await.unwrap();
+                channels.to_game.send(data).unwrap();
+            }
+        });
 
-    loop {
-        tokio::time::sleep(Duration::from_secs(5)).await;
+        tokio::spawn(async move {
+            loop {
+                let data = channels.from_game.recv().await.unwrap();
+                tx.send(data).unwrap();
+            }
+        });
     }
 
-    // let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
-    // println!("[server] Listener started");
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
+    println!("[server] Listener started");
 
-    // loop {
-    //     let (mut stream, _) = listener.accept().await.unwrap();
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        let mut rx = tx.subscribe();
+        let tx = tx.clone();
+        let (mut read_stream, mut write_stream) = stream.into_split();
 
-    //     id += 1;
-    //     let id = id;
-    //     let mut rx = tx.subscribe();
+        tokio::spawn(async move {
+            loop {
+                let value = rx.recv().await.unwrap();
+                let value = bincode::serialize(&value).unwrap();
+                let length = (value.len() as u32).to_be_bytes();
 
-    //     let msg = Message::Connected { id };
-    //     println!("[server] {msg:?}");
-    //     tx.send(msg).unwrap();
+                write_stream.write_all(&length).await.unwrap();
+                write_stream.write_all(&value).await.unwrap();
+            }
+        });
 
-    //     tokio::spawn(async move {
-    //         loop {
-    //             let value = rx.recv().await.unwrap();
-    //             let value = bincode::serialize(&value).unwrap();
-    //             let length = (value.len() as u32).to_be_bytes();
+        tokio::spawn(async move {
+            loop {
+                let mut length = [0; size_of::<u32>()];
+                read_stream.read_exact(&mut length).await.unwrap();
+                let length = u32::from_be_bytes(length);
 
-    //             stream.write_all(&length).await.unwrap();
-    //             stream.write_all(&value).await.unwrap();
-    //         }
-    //     });
-    // }
+                let mut data = vec![0; length as usize];
+                read_stream.read_exact(&mut data).await.unwrap();
+                let data = bincode::deserialize(&data).unwrap();
+
+                tx.send(data).unwrap();
+            }
+        });
+    }
 }
 
 pub async fn client(mut channels: NetChannels) -> Result<()> {
@@ -79,17 +86,13 @@ pub async fn client(mut channels: NetChannels) -> Result<()> {
         }
     });
 
-    tokio::spawn(async move {
-        loop {
-            let data = channels.from_game.recv().await.unwrap();
+    loop {
+        let data = channels.from_game.recv().await.unwrap();
 
-            let data = bincode::serialize(&data).unwrap();
-            let length = (data.len() as u32).to_be_bytes();
+        let data = bincode::serialize(&data).unwrap();
+        let length = (data.len() as u32).to_be_bytes();
 
-            write_stream.write_all(&length).await.unwrap();
-            write_stream.write_all(&data).await.unwrap();
-        }
-    });
-
-    Ok(())
+        write_stream.write_all(&length).await.unwrap();
+        write_stream.write_all(&data).await.unwrap();
+    }
 }
