17c1f695d98117fb7bf42f4df75b27f96c6c3215
JamesBeeProg <JamesBeeProg@gmail.com>
2022-04-06 14:06:59 -0400

    Follow getting started

--- bevy-test/src/main.rs
+++ bevy-test/src/main.rs
@@ -1,5 +1,51 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
+struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Foo".to_string()));
+
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Bar".to_string()));
+
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Baz".to_string()));
+}
+
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("Hello {}!", name.0)
+        }
+    }
 }
