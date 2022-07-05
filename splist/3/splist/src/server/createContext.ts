e91adb1a151072007c3a2942e018595939058b74
jamesBeeProg <jamesbeeprog@gmail.com>
2022-07-04 23:02:13 -0400

    Add useEvent helper for server

--- splist/3/splist/src/server/createContext.ts
+++ splist/3/splist/src/server/createContext.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client';
-import { router } from '@trpc/server';
+import { router, Subscription, SubscriptionEmit } from '@trpc/server';
 import { CreateNextContextOptions } from '@trpc/server/adapters/next';
 import { NodeHTTPCreateContextFnOptions } from '@trpc/server/dist/declarations/src/adapters/node-http';
 import { IncomingMessage } from 'node:http';
 import ws from 'ws';
+import { EventEmitter } from 'node:events';
+import { Events } from '.';
 
 declare global {
     var __prisma: PrismaClient | undefined;
@@ -12,8 +14,22 @@ declare global {
 global.__prisma ??= new PrismaClient();
 const prisma = global.__prisma;
 
+const events = new EventEmitter();
+
+const useEvent = <E extends keyof Events>(
+    event: E,
+    listener: (emit: SubscriptionEmit<Events[E]>, data: Events[E]) => unknown,
+) =>
+    new Subscription<Events[E]>((emit) => {
+        const wrapper = (data: Events[E]) => listener(emit, data);
+        events.on(event, wrapper);
+        return () => events.off(event, wrapper);
+    });
+
 export interface Context {
     prisma: PrismaClient;
+    events: EventEmitter;
+    useEvent: typeof useEvent;
 }
 
 export const createRouter = () => router<Context>();
@@ -25,5 +41,7 @@ type Props =
 export const createContext = async ({ req, res }: Props): Promise<Context> => {
     return {
         prisma,
+        events,
+        useEvent,
     };
 };
--- splist/3/splist/src/server/index.ts
+++ splist/3/splist/src/server/index.ts
@@ -1,15 +1,16 @@
 import { Message } from '@prisma/client';
-import { Subscription } from '@trpc/server';
-import { EventEmitter } from 'stream';
 import { z } from 'zod';
 import { createRouter } from './createContext';
+
+export interface Events {
+    send: Message;
+    clear: null;
+}
 
 const zMessage = z.object({
     id: z.string(),
     content: z.string(),
 });
-
-const events = new EventEmitter();
 
 export const appRouter = createRouter()
     .query('messages', {
@@ -30,37 +31,24 @@ export const appRouter = createRouter()
             const message = await ctx.prisma.message.create({
                 data: input,
             });
-            events.emit('send', message);
+            ctx.events.emit('send', message);
             return message;
         },
     })
     .mutation('clear', {
         async resolve({ ctx }) {
             await ctx.prisma.message.deleteMany();
-            events.emit('clear');
+            ctx.events.emit('clear');
         },
     })
     .subscription('onSend', {
-        resolve() {
-            return new Subscription<Message>((emit) => {
-                const handle = (message: Message) => {
-                    emit.data(message);
-                };
-
-                events.on('send', handle);
-                return () => {
-                    events.off('send', handle);
-                };
-            });
+        resolve({ ctx }) {
+            return ctx.useEvent('send', (emit, message) => emit.data(message));
         },
     })
     .subscription('onClear', {
-        resolve() {
-            return new Subscription((emit) => {
-                const handle = () => emit.data(null);
-                events.on('clear', handle);
-                return () => events.off('clear', handle);
-            });
+        resolve({ ctx }) {
+            return ctx.useEvent('clear', (emit) => emit.data(null));
         },
     });
 
