71999a1750c6e207145769a6f6e8d2c3fc44fb97
jamesBeeProg <jamesbeeprog@gmail.com>
2022-07-05 21:31:00 -0400

    Get AppShell working?

--- splist/3/splist/src/pages/index.tsx
+++ splist/3/splist/src/pages/index.tsx
@@ -2,7 +2,15 @@ import type { NextPage } from 'next';
 import { trpc, useMount } from '../utils/hooks';
 import { useLocalStorage } from '@mantine/hooks';
 import { Messages } from '../components/Messages';
-import { TextInput } from '@mantine/core';
+import {
+    AppShell,
+    Footer,
+    Header,
+    Navbar,
+    TextInput,
+    Title,
+    Text,
+} from '@mantine/core';
 
 const Home: NextPage = () => {
     const [name, setName] = useLocalStorage({
@@ -17,22 +25,38 @@ const Home: NextPage = () => {
     });
 
     return (
-        <>
-            <TextInput
-                label="Username"
-                value={name}
-                onChange={(e) => {
-                    setName(e.target.value);
-                }}
-                onKeyDown={(e) => {
-                    if (e.key !== 'Enter') {
-                        return;
-                    }
-                    login.mutate({ name });
-                }}
-            />
+        <AppShell
+            fixed
+            navbar={
+                <Navbar p="md" width={{ sm: 200, lg: 300 }}>
+                    <Navbar.Section grow>
+                        <Text>Cool navbar</Text>
+                    </Navbar.Section>
+                    <Navbar.Section>
+                        <TextInput
+                            label="Username"
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value);
+                            }}
+                            onKeyDown={(e) => {
+                                if (e.key !== 'Enter') {
+                                    return;
+                                }
+                                login.mutate({ name });
+                            }}
+                        />
+                    </Navbar.Section>
+                </Navbar>
+            }
+            header={
+                <Header height={70} p="md">
+                    <Title>Splist</Title>
+                </Header>
+            }
+        >
             {login.data ? <Messages /> : <p>Logged out</p>}
-        </>
+        </AppShell>
     );
 };
 
