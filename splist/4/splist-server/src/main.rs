089f36f5dbebeaf95e2ddc0992c3c62d9fc436b3
jamesBeeProg <jamesbeeprog@gmail.com>
2022-07-08 22:59:52 -0400

    Setup splist-server websockets

--- splist/4/splist-server/src/main.rs
+++ splist/4/splist-server/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use futures_util::{future, StreamExt, TryStreamExt};
+use log::info;
+use tokio::net::{TcpListener, TcpStream};
+
+mod prisma;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let addr = "127.0.0.1:3000";
+
+    // let client = prisma::new_client().await;
+
+    let listener = TcpListener::bind(addr).await.unwrap();
+    info!("Listening on: {addr}");
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(accept_connection(stream));
+    }
+}
+
+async fn accept_connection(stream: TcpStream) {
+    let addr = stream.peer_addr().unwrap();
+    info!("Peer address: {addr}");
+
+    let ws_stream = tokio_tungstenite::accept_async(stream).await.unwrap();
+    info!("New WebSocket connection: {addr}");
+
+    let (write, read) = ws_stream.split();
+
+    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
+        .forward(write)
+        .await
+        .unwrap();
 }
