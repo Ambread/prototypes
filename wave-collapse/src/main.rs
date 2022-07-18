279a9c0d8e2626c1621c744cc2e3d75dc59e141c
jamesBeeProg <jamesbeeprog@gmail.com>
2022-07-18 16:13:30 -0400

    Factor out into generate_rules

--- wave-collapse/src/main.rs
+++ wave-collapse/src/main.rs
@@ -5,7 +5,7 @@ use std::{
 
 use anyhow::Result;
 use clap::Parser;
-use image::{GenericImageView, Rgba};
+use image::{ImageBuffer, Rgba};
 
 #[derive(Debug, Clone, Parser)]
 struct Args {
@@ -15,33 +15,47 @@ struct Args {
 }
 
 type Pixel = Rgba<u8>;
+type Image = ImageBuffer<Pixel, Vec<u8>>;
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let img = image::io::Reader::open(args.input)?.decode()?.to_rgba8();
+    let relations = generate_rules(&img);
+    dbg!(relations);
+
+    img.save(args.output)?;
+    Ok(())
+}
 
 #[derive(Debug, Clone, Default)]
-struct Relations {
+struct Rule {
+    count: u32,
     up: HashSet<Pixel>,
     down: HashSet<Pixel>,
     left: HashSet<Pixel>,
     right: HashSet<Pixel>,
 }
 
-fn main() -> Result<()> {
-    let args = Args::parse();
-    let img = image::io::Reader::open(args.input)?.decode()?.to_rgba8();
-
-    let mut relations = HashMap::<Pixel, Relations>::new();
+fn generate_rules(img: &Image) -> HashMap<Pixel, Rule> {
+    let mut rules = HashMap::<Pixel, Rule>::new();
 
     for (x, y, pixel) in img.enumerate_pixels() {
-        let relation = relations.entry(*pixel).or_default();
+        let rule = rules.entry(*pixel).or_default();
+        rule.count += 1;
 
-        let directions = [(&mut relation.up, Some(x), y.checked_sub(1))];
+        let directions = [
+            (&mut rule.up, Some(x), y.checked_sub(1)),
+            (&mut rule.down, Some(x), y.checked_add(1)),
+            (&mut rule.left, x.checked_sub(1), Some(y)),
+            (&mut rule.right, x.checked_add(1), Some(y)),
+        ];
 
         for (side, x, y) in directions {
-            if let Some(pixel) = x.zip(y).and_then(|(x, y)| img.get_pixel_checked(x, y)) {
-                side.insert(*pixel);
-            }
+            x.zip(y)
+                .and_then(|(x, y)| img.get_pixel_checked(x, y))
+                .map(|pixel| side.insert(*pixel));
         }
     }
 
-    img.save(args.output)?;
-    Ok(())
+    rules
 }
