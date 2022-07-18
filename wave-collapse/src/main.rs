25df8b0df482d0a6a38aceb1b9cb61a21b801669
jamesBeeProg <jamesbeeprog@gmail.com>
2022-07-18 18:28:50 -0400

    pain

--- wave-collapse/src/main.rs
+++ wave-collapse/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    cell::RefCell,
     collections::{HashMap, HashSet},
     path::PathBuf,
 };
@@ -16,6 +17,7 @@ struct Args {
 }
 
 type Pixel = Rgba<u8>;
+type Tile = HashSet<Pixel>;
 type Image = ImageBuffer<Pixel, Vec<u8>>;
 
 fn main() -> Result<()> {
@@ -35,38 +37,51 @@ fn main() -> Result<()> {
 
     let mut rng = thread_rng();
 
-    while let Some((x, y, tile)) = find_lowest_entropy(&mut tiles, &rules) {
-        let collapsed = {
-            let pixels: Vec<_> = tile.iter().collect();
+    loop {
+        if let Some((x, y, tile)) = find_lowest_entropy(&mut tiles, &rules) {
+            let collapsed = {
+                let pixels: Vec<_> = tile.iter().collect();
 
-            let weights = pixels.iter().map(|pixel| rules[pixel].count);
-            let dist = WeightedIndex::new(weights)?;
+                let weights = pixels.iter().map(|pixel| rules[pixel].count);
+                let dist = WeightedIndex::new(weights)?;
 
-            *pixels[dist.sample(&mut rng)]
-        };
+                *pixels[dist.sample(&mut rng)]
+            };
 
-        tile.retain(|pixel| *pixel == collapsed);
-        output.put_pixel(x as u32, y as u32, collapsed);
+            tile.retain(|pixel| *pixel == collapsed);
+            output.put_pixel(x as u32, y as u32, collapsed);
+        } else {
+            break;
+        }
+
+        for (x, y, tile) in enumerate_pixels_mut(&mut tiles) {
+            for pixel in tile.iter() {
+                let rule = &rules[pixel];
+
+                let directions = [
+                    (&rule.up, Some(x), y.checked_sub(1)),
+                    (&rule.down, Some(x), y.checked_add(1)),
+                    (&rule.left, x.checked_sub(1), Some(y)),
+                    (&rule.right, x.checked_add(1), Some(y)),
+                ];
+
+                for (side, x, y) in directions {
+                    let t = x.zip(y).and_then(|(x, y)| tiles.get_mut(x)?.get_mut(y));
+                }
+            }
+        }
     }
 
-    output.save("wave-collapse-output.png")?;
+    output.save("dev/wave-collapse-output.png")?;
 
     Ok(())
 }
 
 fn find_lowest_entropy<'a>(
-    tiles: &'a mut [Vec<HashSet<Pixel>>],
+    tiles: &'a mut [Vec<Tile>],
     rules: &'a HashMap<Pixel, Rule>,
-) -> Option<(usize, usize, &'a mut HashSet<Pixel>)> {
-    tiles
-        .iter_mut()
-        .enumerate()
-        .flat_map(|(x, column)| {
-            column
-                .iter_mut()
-                .enumerate()
-                .map(move |(y, tile)| (x, y, tile))
-        })
+) -> Option<(usize, usize, &'a mut Tile)> {
+    enumerate_pixels_mut(tiles)
         .filter(|(_, _, tile)| tile.len() > 1)
         .map(|(x, y, tile)| {
             let entropy = shannon_entropy_for_tile(tile, rules);
@@ -76,7 +91,18 @@ fn find_lowest_entropy<'a>(
         .map(|(x, y, tile, _)| (x, y, tile))
 }
 
-fn shannon_entropy_for_tile(tile: &HashSet<Pixel>, rules: &HashMap<Pixel, Rule>) -> f64 {
+fn enumerate_pixels_mut(
+    tiles: &mut [Vec<Tile>],
+) -> impl Iterator<Item = (usize, usize, &mut Tile)> {
+    tiles.iter_mut().enumerate().flat_map(|(x, column)| {
+        column
+            .iter_mut()
+            .enumerate()
+            .map(move |(y, tile)| (x, y, tile))
+    })
+}
+
+fn shannon_entropy_for_tile(tile: &Tile, rules: &HashMap<Pixel, Rule>) -> f64 {
     let sum = tile.iter().map(|pixel| rules[pixel].count).sum::<f64>();
     -tile
         .iter()
