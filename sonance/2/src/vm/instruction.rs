4f5034633d4714f1e33ae5a200954c389c669d96
jamesBeeProg <jamesbeeprog@gmail.com>
2022-07-27 19:11:57 -0400

    Impl Display for Instruction

--- sonance/2/src/vm/instruction.rs
+++ sonance/2/src/vm/instruction.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Instruction {
     Halt,
@@ -34,4 +36,45 @@ pub enum Instruction {
     Eq,
     Gt,
     Geq,
+}
+
+impl Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Instruction::Halt => write!(f, "halt"),
+
+            Instruction::Push(value) => write!(f, "push {value}"),
+            Instruction::Pop => write!(f, "pop"),
+            Instruction::Dupe => write!(f, "dupe"),
+
+            Instruction::Jump => write!(f, "jump"),
+            Instruction::JumpIf => write!(f, "jump_if"),
+
+            Instruction::Load(key) => write!(f, "load {key}"),
+            Instruction::Store(key) => write!(f, "store {key}"),
+
+            Instruction::Call(index) => write!(f, "call {index}"),
+            Instruction::Return => write!(f, "return"),
+
+            Instruction::In => write!(f, "in"),
+            Instruction::Out => write!(f, "out"),
+
+            Instruction::Add => write!(f, "add"),
+            Instruction::Sub => write!(f, "sub"),
+            Instruction::Mul => write!(f, "mul"),
+            Instruction::Div => write!(f, "div"),
+
+            Instruction::BitAnd => write!(f, "bit_and"),
+            Instruction::BitOr => write!(f, "bit_or"),
+            Instruction::BitNot => write!(f, "bit_not"),
+
+            Instruction::BoolAnd => write!(f, "bool_and"),
+            Instruction::BoolOr => write!(f, "bool_or"),
+            Instruction::BoolNot => write!(f, "bool_not"),
+
+            Instruction::Eq => write!(f, "eq"),
+            Instruction::Gt => write!(f, "gt"),
+            Instruction::Geq => write!(f, "geq"),
+        }
+    }
 }
