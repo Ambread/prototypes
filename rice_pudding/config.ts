c8b02f8d86cd4207946297b136fe016f33947239
JamesBeeProg <JamesBeeProg@gmail.com>
2022-09-14 08:54:51 -0400

    Oops

--- rice_pudding/config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from './src/config';
-
-export const config: Config = {
-    token: 'OTE5NzQzODg4MjY3MjgwNDQ0.GD-aS0.gtPNaW6BY4HV1Wp9OVmsYoY8OI4nutLVDj88Hs',
-    clientId: '919743888267280444',
-    guildIds: ['659466154414440451', '684237072827154435'],
-};
