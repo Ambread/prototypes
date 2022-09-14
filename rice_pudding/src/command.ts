8b6c88eae5ddf6bbd18e3947387b00ed6448bb94
JamesBeeProg <JamesBeeProg@gmail.com>
2022-09-14 14:10:53 -0400

    a

--- rice_pudding/src/command.ts
+++ rice_pudding/src/command.ts
@@ -3,6 +3,7 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 
 export interface Command {
     builder: Pick<SlashCommandBuilder, 'toJSON' | 'setName'>;
+
     execute(
         interaction: ChatInputCommandInteraction,
         prisma: PrismaClient,
@@ -13,7 +14,7 @@ import { give } from './commands/give';
 import { wallet } from './commands/wallet';
 import { top } from './commands/top';
 
-export const commands: Record<string, Command> = { give, wallet, top };
+export const commands = { give, wallet, top };
 
 for (const [name, command] of Object.entries(commands)) {
     command.builder.setName(name);
