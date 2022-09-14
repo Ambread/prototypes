39bd74d24c3ca5271c1f197cec301d915cdda989
JamesBeeProg <JamesBeeProg@gmail.com>
2022-09-14 09:52:59 -0400

    Add username storage

--- rice_pudding/src/commands/give.ts
+++ rice_pudding/src/commands/give.ts
@@ -7,32 +7,32 @@ export const give: Command = {
     ),
 
     async execute(interaction, prisma) {
-        const senderId = interaction.user.id;
-        const sender = await prisma.user.upsert({
-            where: { id: senderId },
-            update: {},
-            create: { id: senderId },
+        const sender = interaction.user;
+        const senderData = await prisma.user.upsert({
+            where: { id: sender.id },
+            update: { username: sender.username },
+            create: { id: sender.id, username: sender.username },
         });
 
-        if (sender.pudding === 0) {
+        if (senderData.pudding === 0) {
             return interaction.reply('You have no pudding left to give!');
         }
 
-        const receiverId = interaction.options.getUser('person', true).id;
-        const receiver = await prisma.user.upsert({
-            where: { id: receiverId },
-            update: {},
-            create: { id: receiverId },
+        const receiver = interaction.options.getUser('person', true);
+        const receiverData = await prisma.user.upsert({
+            where: { id: receiver.id },
+            update: { username: receiver.username },
+            create: { id: receiver.id, username: receiver.username },
         });
 
         await prisma.$transaction([
             prisma.user.update({
-                where: { id: senderId },
-                data: { pudding: sender.pudding - 1 },
+                where: { id: sender.id },
+                data: { pudding: senderData.pudding - 1 },
             }),
             prisma.user.update({
-                where: { id: receiverId },
-                data: { pudding: receiver.pudding + 1 },
+                where: { id: receiver.id },
+                data: { pudding: receiverData.pudding + 1 },
             }),
         ]);
     },
