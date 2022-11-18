6cdc1f6b31d3a4c33ffa900b57c7930004b7d2f2
jamesBeeProg <jamesbeeprog@gmail.com>
2022-11-17 20:20:24 -0500

    Start with eval

--- math/src/main.rs
+++ math/src/main.rs
@@ -1,63 +1,122 @@
 fn main() {
-    println!("Hello, world!");
+    let f = 6.mul(X.exp(2)).add(17.mul(X)).add(12);
+    dbg!(f.eval(5.0));
 }
 
-macro_rules! op_struct {
-    ($($ident:ident => $op:tt),*) => {$(
-        #[derive(Debug, Clone, Copy)]
-        struct $ident<L, R>(L, R);
+use Expr::X;
 
-        impl<L, R> Value for $ident<L, R>
-        where
-            L: Value,
-            R: Value,
-        {
-            fn at(&self, x: f64) -> f64 {
-                self.0.at(x) $op self.1.at(x)
-            }
+enum Expr {
+    X,
+    Const(f64),
+    UnaryOp(UnaryOp, Box<Expr>),
+    BiOp(Box<Expr>, BinaryOp, Box<Expr>),
+}
+
+impl Expr {
+    fn eval(self, x: f64) -> f64 {
+        match self {
+            X => x,
+            Expr::Const(c) => c,
+            Expr::UnaryOp(op, a) => op.eval(a.eval(x)),
+            Expr::BiOp(a, op, b) => op.eval(a.eval(x), b.eval(x)),
         }
-    )*};
-}
-
-op_struct!(Add => +, Sub => -, Mul => *, Div => /);
-
-macro_rules! impl_ops {
-    ($($ident:ident $(< $($generic:ident),+ >)? $(,)* )+) => {$(
-        impl<__R: Value, $($($generic),+)? > std::ops::Add<__R> for $ident $(< $($generic),+ >)? {
-            type Output = Add<Self, __R>;
-            fn add(self, rhs: __R) -> Self::Output {
-                Add(self, rhs)
-            }
-        }
-    )*};
-}
-
-impl_ops!(X, Const, Add<L, R>);
-
-macro_rules! operators {
-    () => {};
-}
-
-operators!();
-
-trait Value {
-    fn at(&self, x: f64) -> f64;
-}
-
-#[derive(Debug, Clone, Copy)]
-struct X;
-
-impl Value for X {
-    fn at(&self, x: f64) -> f64 {
-        x
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-struct Const(f64);
+enum UnaryOp {
+    Neg,
+    Sin,
+    Cos,
+}
 
-impl Value for Const {
-    fn at(&self, _: f64) -> f64 {
-        self.0
+impl UnaryOp {
+    fn eval(self, a: f64) -> f64 {
+        match self {
+            UnaryOp::Neg => -a,
+            UnaryOp::Sin => a.sin(),
+            UnaryOp::Cos => a.cos(),
+        }
+    }
+}
+
+enum BinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Pow,
+}
+
+impl BinaryOp {
+    fn eval(self, a: f64, b: f64) -> f64 {
+        match self {
+            BinaryOp::Add => a + b,
+            BinaryOp::Sub => a - b,
+            BinaryOp::Mul => a * b,
+            BinaryOp::Div => a / b,
+            BinaryOp::Pow => a.powf(b),
+        }
+    }
+}
+
+trait ToExpr: Sized {
+    fn to_expr(self) -> Expr;
+
+    fn add(self, rhs: impl ToExpr) -> Expr {
+        Expr::BiOp(
+            Box::new(self.to_expr()),
+            BinaryOp::Add,
+            Box::new(rhs.to_expr()),
+        )
+    }
+
+    fn sub(self, rhs: impl ToExpr) -> Expr {
+        Expr::BiOp(
+            Box::new(self.to_expr()),
+            BinaryOp::Add,
+            Box::new(rhs.to_expr()),
+        )
+    }
+
+    fn mul(self, rhs: impl ToExpr) -> Expr {
+        Expr::BiOp(
+            Box::new(self.to_expr()),
+            BinaryOp::Add,
+            Box::new(rhs.to_expr()),
+        )
+    }
+
+    fn div(self, rhs: impl ToExpr) -> Expr {
+        Expr::BiOp(
+            Box::new(self.to_expr()),
+            BinaryOp::Add,
+            Box::new(rhs.to_expr()),
+        )
+    }
+
+    fn exp(self, rhs: impl ToExpr) -> Expr {
+        Expr::BiOp(
+            Box::new(self.to_expr()),
+            BinaryOp::Add,
+            Box::new(rhs.to_expr()),
+        )
+    }
+}
+
+impl ToExpr for Expr {
+    fn to_expr(self) -> Expr {
+        self
+    }
+}
+
+impl ToExpr for f64 {
+    fn to_expr(self) -> Expr {
+        Expr::Const(self)
+    }
+}
+
+impl ToExpr for i32 {
+    fn to_expr(self) -> Expr {
+        Expr::Const(self as f64)
     }
 }
